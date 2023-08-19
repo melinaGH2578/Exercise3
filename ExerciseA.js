6b2d39c372bae158bdac304b87fccd103d609a7b
melinaGH2578 <142569776+melinaGH2578@users.noreply.github.com>
2023-08-19 21:56:13 +0330

    Add files via upload

---
+++ ExerciseA.js
@@ -0,0 +1,11 @@
+const number = prompt(" enter a number:");
+
+let num = 0;
+while (num < number) {
+    if (num % 2 == 0 ) {
+        alert(num);
+
+    }
+    num++;
+
+}
---
+++ ExerciseB.js
@@ -0,0 +1,6 @@
+const firstname = prompt(" enter your firstname");
+const lastname = prompt(" enter your lastname");
+
+
+let names = firstname + lastname;
+alert( names.length);
